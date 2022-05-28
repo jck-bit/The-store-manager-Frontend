314dfd36462cc18d7e580f096d53416443889d78
jck-bit <jckkinyanjui@gmail.com>
2022-05-28 19:36:41 +0300

    devops +++

---
+++ Home page/home.js
@@ -0,0 +1,12 @@
+document.getElementById("submit").addEventListener("click", function() {
+    window.location.href = "../sales/sales.html";
+  })
+
+document.getElementById("holla").addEventListener("click", function() {
+    window.location.href = "../products/product.html"
+  
+})
+
+document.getElementById("cart").addEventListener('click', function(){
+    window.location.href = "../cart/Cart.html"
+})
---
+++ Login/login.js
@@ -0,0 +1,3 @@
+document.getElementById("submit").addEventListener("click", function() {
+    window.location.href = "../Home page/index.html";
+  })
