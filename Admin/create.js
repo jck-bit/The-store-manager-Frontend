4dfa91de47d77c9f64483a8b640cb6bce55da04e
jck-bit <jckkinyanjui@gmail.com>
2022-05-29 17:40:42 +0300

    admin-create

--- Admin/create.js
+++ Admin/create.js
@@ -1,8 +1,23 @@
 function buttonClicked(){
-  window.location.href = "../Products/product.html"
+  window.location.href = "./admin.html";
 }
 
 window.onload=function(){
 var btn = document.getElementById("edit");
 btn.addEventListener("click", buttonClicked, true);
 }
+
+
+// function Hello(){
+//   window.location.href = "../sales/sales.html";
+// }
+
+// window.onload=function(){
+// var btn = document.getElementById("sales");
+// btn.addEventListener("click", Hello, true);
+// }
+
+
+document.getElementById("sales").addEventListener("click", function(){
+  window.location.href = "../sales/sales.html";
+});
