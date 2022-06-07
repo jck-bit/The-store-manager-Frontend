8bee799ba5e29b17f405c47e28198596929e5bdd
jck-bit <jckkinyanjui@gmail.com>
2022-06-07 14:49:12 +0300

    Navbar on products page-#182401325

--- Products/product.js
+++ Products/product.js
@@ -1,3 +1,8 @@
 document.getElementById("back").addEventListener('click', function(){
     window.location.href = "../Home page/Home.html"
 })
+
+document.getElementById("logout").addEventListener('click', function(){
+    window.location.href = "../index.html"
+})
+
