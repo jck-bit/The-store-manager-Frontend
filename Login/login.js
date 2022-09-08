3fcd2ea58b9e801d4a051fe8862ad249111b703a
jck-bit <jckkinyanjui@gmail.com>
2022-09-08 17:14:26 +0300

    removed server from app-fetching data from heroku now

--- Login/login.js
+++ Login/login.js
@@ -1,3 +1,4 @@
+
 // document.getElementById("submit").addEventListener("click", function() {
 //   window.location.href = "./Home page/Home.html";
 // })
@@ -6,33 +7,75 @@ document.getElementById("login").addEventListener("click", function() {
  window.location.href = "./Admin/Login.html";
 });
 
-//get the users input from the login page and check if the name and password are same as the ones in the database if so then redirect to the home page if not then display an error message
-document.getElementById("submit").addEventListener("click", function() {
-  var name = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  var url = 'http://localhost:5000/login';
-  var data = {
-    name: name,
-    password: password
-  };
-  var response = fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json'
-    }})
 
-    console.log(response)
+document.getElementById('submit').addEventListener('click', function() {
+  var username = document.getElementById('username').value
+  var password = document.getElementById('password').value
+  
 
-    .then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    if (data.success) {
-      window.location.href = "./Admin/Home.html";
+  // var url = 'http://localhost:5000/login';
 
-    } else {
-     alert("Wrong username or password");
-    }
-  });
-}
-);
+  // fetch(url,{
+  //   method:"POST",
+  //   headers:{
+  //     "content-Type":"application/json",
+  //   },
+  //   body:JSON.stringify({
+  //     username,
+  //     password
+  //   }),
+  // })
+  // .then(res=>res.json())
+  // .then(data => console.log(data))
+
+  const opt ={
+    method :"POST",
+    headers:{
+      'Accept': 'application/json, text/plain, */*',
+      'Content-type': 'application/json'
+    },
+    body:JSON.stringify({
+      username:username,
+      password:password
+    })
+  }
+
+  fetch( 'http://localhost:5000/login', opt)
+  .then(res=> {
+    if (res.status == 200) return res.json()
+    else alert("There has been some errors")
+  })
+  .then()
+  .catch(console.error("there was an error"))
+
+})
+
+
+
+// document.getElementById("submit").addEventListener("click", function() {
+//   var username = document.getElementById("username").value;
+//   var password = document.getElementById("password").value;
+//   var url = 'http://localhost:5000/login';
+//   var data = {
+//     username: username,
+//     password: password
+//   };
+//       var response = fetch(url, {
+//         method: 'POST',
+//         body: JSON.stringify(data),
+//         headers: {
+//          'Content-Type': 'application/json'
+//           }})
+
+//     console.log(response) .then(function(response) {
+//     return response.json();
+//   }).then(function(data) {
+//     if (data.success) {
+//       window.location.href = "./Admin/Home.html";
+
+//     } else {
+//      alert("Wrong username or password");
+//     }
+//   });
+// }
+// );
