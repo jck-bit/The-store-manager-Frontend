141388445e14d3e4a6e98d182489b34a4ec83cb3
jck-bit <jckkinyanjui@gmail.com>
2022-09-16 18:26:14 +0300

    updated users

--- static/login.js
+++ static/login.js
@@ -4,8 +4,7 @@ login = () => {
   
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Cookie", "session=.eJwlzsENwzAIAMBd_O4D7AAmy0TGgNqv07yq7t5IHeCk-5QjV5zPsr_XFY9yvLzsxQabdA5k9VYtgM2TIEFym1OzDSHf0MZIYLbsCn2o-KhuRKBxIzHmqAYpraOqVpo2tHJzUhbxYGJ2og0xtaUzZADihNms3JHrjPXfYPn-AP3EL30.YyRxpg.e5Ta_R1W2w5wtW-qToRcBJGuQKY");
-  
+ 
   var raw = JSON.stringify({
     "username": username,
     "password": password
@@ -18,9 +17,19 @@ login = () => {
     redirect: 'follow'
   };
   
-  fetch("http://127.0.0.1:5000/login", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
+  fetch("https://flask-app-store-manager.herokuapp.com/login", requestOptions)
+    .then(response => response.json())
+    .then((result) =>{
+      if (result.status === 'Failed'){
+        document.getElementById('danger').style.color='red'
+        document.getElementById('danger').innerHTML = result.message
+      }
+      if (result.status === 'success'){
+        window.location.assign("./home/home.html")
+      }
+      window.localStorage.setItem('token', result.token)
+      console.log(result)
+    })
     .catch(error => console.log('error', error));
    
 }
